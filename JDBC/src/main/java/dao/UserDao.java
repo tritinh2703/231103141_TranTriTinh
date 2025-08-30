d54c2693d433b602d609c901297925b338483cdc
tritinh <kayzaygaming685@gmail.com>
2025-08-30 14:51:41 +0700

    Delete JDBC directory

--- JDBC/src/main/java/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import model.User;
-
-public interface UserDao {
-    User get(String username) throws Exception;
-}
--- JDBC/src/main/java/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import model.User;
-
-public interface UserService {
-    User login(String username, String password) throws Exception;
-}
